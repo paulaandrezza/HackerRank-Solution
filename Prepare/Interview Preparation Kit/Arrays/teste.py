33d4baf47454fc89bbdc4905bfb47db78c03bd98
Paula Marinho <paulaandrezza25@gmail.com>
2022-11-18 17:01:06 -0300

    add 2dArray-DS.py

--- Prepare/Interview Preparation Kit/Arrays/teste.py
+++ Prepare/Interview Preparation Kit/Arrays/teste.py
@@ -1,13 +1,17 @@
-a = [1, 2, 3, 4, 5]
-d = 2
+arr = [ [-9, -9, -9,  1, 1, 1], 
+        [ 0, -9,  0,  4, 3, 2],
+        [-9, -9, -9,  1, 2, 3],
+        [ 0,  0,  8,  6, 6, 0],
+        [ 0,  0,  0, -2, 0, 0],
+        [ 0,  0, 1,  2, 4, 0 ]] 
 
-b = []
+maior = -100
 
-for i in range(len(a)):
-    if(i+d >= len(a)):
-        print("if ", a[i+d-len(a)])
-        b.append(a[i+d-len(a)])
-    else:
-        print(a[i+d])
-        b.append(a[i+d])
-print(b)
+for i in range(4):
+    for j in range(4):
+        s = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+        print(s)
+        if s > maior:
+            maior = s
+
+print(maior)
